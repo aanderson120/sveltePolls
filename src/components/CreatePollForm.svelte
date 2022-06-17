6227f9ca6540ef5f51280842e32da2de9d37f54e
Amanda Anderson <aanderson_120@yahoo.com>
2022-06-17 11:45:18 -0500

    tutorial 21 form component

---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,43 @@
+<script>
+  let fields = { question: "", answerA: "", answerB: "" };
+  const sumbitHandler = () => {
+    console.log(fields);
+  };
+</script>
+
+<form on:submit|preventDefault={sumbitHandler}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" bind:value={fields.question} />
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Answer A:</label>
+    <input type="text" id="answer-a" bind:value={fields.answerA} />
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Answer B:</label>
+    <input type="text" id="answer-b" bind:value={fields.answerB} />
+  </div>
+  <button>Add Poll</button>
+</form>
+
+<style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .form-field {
+    margin: 18px auto;
+  }
+  input {
+    height: 25px;
+    width: 100%;
+    border-radius: 6px;
+    border: none;
+}
+  label {
+    margin: 10px auto;
+    text-align: left;
+  }
+</style>
